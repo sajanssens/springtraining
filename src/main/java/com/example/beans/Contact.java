cc82c0046218a6f99be7b510b6445e95f3bc0712
Bram Janssens <s.a.janssens@gmail.com>
2019-10-10 09:31:39 +0200

    Unittest modified with mocks (2).

---
+++ src/main/java/com/example/beans/Contact.java
@@ -0,0 +1,10 @@
+package com.example.beans;
+
+public class Contact {
+
+    private String naam;
+
+    public Contact(String n) { this.naam = n; }
+
+    public String getNaam() { return naam; }
+}
